b030ae9d186c008db84117a48b8f836d5a570d4d
Mark Jones <markjones7@mail.weber.edu>
2019-01-31 09:19:26 -0700

    initial commit

---
+++ mod4/month_day.c
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  month_day.c
+ *
+ *    Description:  take month and day from user, and verify entires are valid
+ *
+ *        Version:  1.0
+ *        Created:  01/31/2019 09:17:09 AM
+ *       Revision:  none
+ *       Compiler:  gcc month_day.c -o month_day.out
+ *
+ *         Author:  Mark Jones (), markjones7@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    // capture usser info
+    //
+    // use if to validate month
+    //
+    // 3) use switch to validate day of the month
+    return 0;
+}
+// Function Definitions
+
+
