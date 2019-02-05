e675bb1c31183750224e3eed6339abdba1891b3b
Mark Jones <markjones7@mail.weber.edu>
2019-02-05 09:16:17 -0700

    Task 3 add request for number of entrieses from usser

--- mod5/firstwhile.c
+++ mod5/firstwhile.c
@@ -18,18 +18,24 @@
 #include <stdio.h>
 
 // Constants
-#define MAXCOUNT 4
+#define MINCOUNT 2
+#define MAXCOUNT 5
 // Function Prototypes
 
 // Main Function
 int main()
 {
     //task 1; calcualte the average of user values
-    //taks 2; validate user input for 0 to 100 only
+    //task 2; validate user input for 0 to 100 only
+    //task 3; ask user how many values they need to enter
 
     int count = 0;
     float grade, avg;
     float total = 0;
+    int student_count = 0;
+
+    printf("How many hw values do you want to enter?(between %d and %d) \n",MINCOUNT, MAXCOUNT);
+    scanf("%d",&student_count);
     while(count < MAXCOUNT)
     {
         printf("\nEnter %d homework grade(0-100): ", count+1);
@@ -42,6 +48,10 @@ int main()
         total += grade;     //add up grades
       //update test condition: sentinel
         count++;
+        if (count == student_count)
+        {
+            break;      //reacht he student_count
+        }
     }
     avg = total/MAXCOUNT;
     printf("Your average is [%6.2f]\n",avg);
