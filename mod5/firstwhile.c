827868d9897192ca44c3f179d85f55afa1b26c55
Mark Jones <markjones7@mail.weber.edu>
2019-02-05 09:01:21 -0700

    Task 2 validate user input

--- mod5/firstwhile.c
+++ mod5/firstwhile.c
@@ -25,6 +25,8 @@
 int main()
 {
     //task 1; calcualte the average of user values
+    //taks 2; validate user input for 0 to 100 only
+
     int count = 0;
     float grade, avg;
     float total = 0;
@@ -32,7 +34,11 @@ int main()
     {
         printf("\nEnter %d homework grade(0-100): ", count+1);
         scanf("%f", &grade);
-
+    if(grade<0 || grade > 100)
+    {
+        printf("Invalid input please try again\n");
+        continue;
+    }
         total += grade;     //add up grades
       //update test condition: sentinel
         count++;
