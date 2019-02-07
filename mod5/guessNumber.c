3300506804d8d745309b6cd852558ee30aaed3b9
Mark Jones <markjones7@mail.weber.edu>
2019-02-07 09:18:13 -0700

    Add guess messages. Change limits. Add go higher and go lower hints

--- mod5/guessNumber.c
+++ mod5/guessNumber.c
@@ -20,7 +20,8 @@
 #include <time.h>   //time()
 
 // Constants
-#define MAX 3
+#define MAX 5
+#define LIMIT 100
 // Function Prototypes
 
 // Main Function
@@ -28,16 +29,26 @@ int main()
 {
     int number, guess;
     srand(time(0));     //set the random SEED
-    guess = rand() % 10; //set random number between 0-9
+    guess = rand() % LIMIT; //set random number between 0-9
     //int i; //before C89 you need to define i outside loop
     for(int i = 0; i< MAX; i++)
     {
-        printf("Please guess a number between 0 and 10\n");
+        printf("Please guess a number between 0 and %d\n",LIMIT - 1);
+       printf("You have %d opportunities left\n", MAX -i);
         scanf("%d", &number);
-        if(number == guess)
+
+        if(number > guess)
+        {
+            printf("Go lower\n");
+        }
+        else if( number < guess)
+        {
+            printf("Go higher\n");
+        }
+        else
         {
             printf("Congrats, you got the correct number\n");
-            break;
+            return 0;
         }
     } //end of for loop
     printf("Sorry, you did not guess the [%d] number\nPlay again\n",guess);
