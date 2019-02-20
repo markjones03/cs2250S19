6fdd2967d344e159af78143585be88a1b9b3ce84
Mark Jones <markjones7@mail.weber.edu>
2019-02-19 23:31:16 -0700

    player collect done, menu half done

--- homework/main_soccer2.c
+++ homework/main_soccer2.c
@@ -26,11 +26,22 @@ int main()
 {
 int jerseyNum[5];
 int playerRating[5];
-//int i;
 int j;
+int newNum;
+int newRating;
+int newJersey;
+int replaceJersey;
+char choice;
 
-//for(i = 0; i < 1 ; ++i)
-
+   // printf("MENU\n");
+   // printf("u - Update Player rating\n");
+   // printf("a - Output players above a rating\n");
+   // printf("r - Replace player\n");
+   // printf("o - Output roster\n");
+   // printf("q - Quit\n");
+   // printf("\nChoose an option:\n");
+   // scanf("%c", &choice);
+   //
         for(j = 1; j <= NUM_PLAYERS; ++j)
     {
             printf("Enter player %d's jersey number(0-99): \n",j);
@@ -41,13 +52,81 @@ int j;
          
     }
      
-    printf("ROSTER\n");
+    printf("\nROSTER\n");
     for(j=1; j<= NUM_PLAYERS; ++j)
     {
         printf("Player %d -- Jersey number: %d, Rating: %d \n", j, jerseyNum[j],playerRating[j]);
     }
-
     
+
+    printf("\nMENU\n");
+    printf("u - Update Player rating\n");
+    printf("a - Output players above a rating\n");
+    printf("r - Replace player\n");
+    printf("o - Output roster\n");
+    printf("q - Quit\n");
+    printf("\nChoose an option:\n");
+    scanf("%s", &choice);
+
+    if(choice == 'q')
+    {
+        printf("Good Bye\n");
+        return 0;
+    }
+
+    else if(choice == 'u')
+    {
+        printf("Enter a jersey number: \n");
+        scanf("%d", &newNum);
+
+        if( newNum == jerseyNum[j])
+        {
+        
+        printf("Enter a new rating for player: \n");
+        scanf("%d", &newRating);
+
+            playerRating[j] = newRating;
+        }
+        
+    }
+    else if(choice == 'r')
+    {
+        printf("Enter a jersey number:\n");
+        scanf("%d", &replaceJersey);
+        if(replaceJersey == jerseyNum[j])
+            {
+                printf("Enter a new jersey number:\n");
+                scanf("%d", &newJersey);
+                printf("Enter a rating for the new player:\n");
+                scanf("%d", &newRating);
+
+            }
+
+    }
+
+    else if(choice == 'o')
+    {
+        printf("ROSTER\n");
+         for(j=1; j<= NUM_PLAYERS; ++j)
+         {
+        printf("Player %d -- Jersey number: %d, Rating: %d \n", j, jerseyNum[j],playerRating[j]);
+         }
+    
+    printf("MENU\n");
+    printf("u - Update Player rating\n");
+    printf("a - Output players above a rating\n");
+    printf("r - Replace player\n");
+    printf("o - Output roster\n");
+    printf("q - Quit\n");
+    printf("\nChoose an option:\n");
+    scanf("%s", &choice);
+if(choice == 'q')
+    {
+        printf("Good Bye\n");
+        return 0;
+    }
+    } //end else if choice o - output roster
+
 
 
     return 0;
