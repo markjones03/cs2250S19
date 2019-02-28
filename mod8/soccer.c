9c0a2ba7515a29a570655b2c85baf3a9b3a0dc99
Mark Jones <markjones7@mail.weber.edu>
2019-02-28 09:54:41 -0700

    Initial commit

---
+++ mod8/soccer.c
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  soccer.c
+ *
+ *    Description:  Implimentation file of the soccer library
+ *                  this file does not have a main{} function
+ *                  no prototypes
+ *                  maybe some local constants
+ *                  include your header file "soccer.h"
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2019 09:44:38 AM
+ *       Revision:  none
+ *       Compiler:
+            Usage: 
+ *
+ *         Author:  Mark Jones (), markjones7@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "soccer.h" // header file
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
---
+++ mod8/soccer.h
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  soccer.h
+ *
+ *    Description:  n
+ *
+ *        Version:  1.0
+ *        Created:  02/28/2019 09:43:57 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Mark Jones (), markjones7@mail.weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#ifndef  SOCCER__INC__
+#define  SOCCER__INC__
+    <+DETAILED+>
+//constants
+#define NUM_PLAYERS 2
+
+//function prototypes
+//all function types
+
+
+
+
+#endif /* ----- #ifndef SOCCER__INC__ ----- */
+
