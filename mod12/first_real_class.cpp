b200d3a3fc43256998c29ee8f663d09791d3d8d5
Mark Jones <markjones7@mail.weber.edu>
2019-04-04 10:16:37 -0600

    add logic to populate a vectore and display the results

--- mod12/first_real_class.cpp
+++ mod12/first_real_class.cpp
@@ -36,15 +36,43 @@ int main(int argc, char* argv[])
 //    Movie movies[10]; //array of 10 movies fixed to that size 10 spots
     vector<Movie> movies;  //define a vector of movies. Grow on demand
     char another = 'y';
+    
     // Loop for entries
     while(tolower(another) == 'y')
     {
+        Movie movie;
+        string title;
+        int year;
+        //Get user input
+        cout << "Title: ";  //get title
+        getline(cin, title);
+        movie.set_title(title);
+
+        cout << "Year: ";  //get year
+        cin >> year;
+        movie.set_year(year);
+        // Add it to the vector
+        movies.push_back(movie);
+
+
+
         cout << endl << "Enter another movie? [y/n]: ";
         cin >> another;
         cin.ignore();  // ignore anything else in the buffer
         cout << endl;
     }// end of while looop
-
+    //Display Movie
+    
+    const int w = 10;
+    cout << left
+        << setw(w*3) << "TITLE"
+        << setw(w)<< "YEAR" << endl;
+    for(Movie movie : movies)  // looping over vector
+    {
+    cout << setw(w*3) << movie.get_title()
+        << setw(w) << movie.get_year() << endl;
+    }
+    cout << endl;
 
 
     return 0;
