1823a944b9ff8cf018d1be2dffbfc4fff7228ca9
Mark Jones <markjones7@mail.weber.edu>
2019-04-09 10:10:20 -0600

    Add menu and read movies from file

--- mod13/movie_list.cpp
+++ mod13/movie_list.cpp
@@ -18,19 +18,62 @@
  * =====================================================================================
  */
 #include <iostream>
+#include <iomanip> //pretty output
+#include <vector> // for vectors
+#include <string> //for strings
+#include <fstream> //for file stream
+#include <sstream> //to read strings with spaces
 #include "Movie.h"
 using namespace std;
 
-// Constants
-
+// Constants & Globals
+const string movie_file = "movies.txt";
 // Function Prototypes
-
+void display_menu();
+vector<Movie> read_movies_from_file();
 // Main Function
 int main(int argc, char* argv[])
 {
+    cout << "The Movie List Program\n" << endl;
+    display_menu();
+    vector<Movie> movies = read_movies_from_file();
 
     return 0;
 }
 // Function Definitions
 
+void display_menu()
+{
+    cout << "COMMANDS" << endl
+        << "v - View movie list" << endl
+        << "a - Add a movie" << endl
+        << "d - Delete a movie" << endl
+        << "x - Exit" << endl << endl;
+}
 
+
+vector<Movie> read_movies_from_file()
+{
+    vector<Movie> movies;
+    // Read File
+    ifstream input_file(movie_file);
+    if(input_file) // if file opened succssfully
+    {
+        string line;
+        while(getline(input_file, line))
+                {
+                    stringstream ss(line);  // read the line and cast it as string
+                    // Now ready to parse the line
+                    string title;
+                    int year, stars;
+                    getline(ss, title, ',');  //read title until you get a tab "\t"
+                    ss >> year >> stars;       // get year and stars
+//                cout << title << " " << year " " << stars << "extra" << endl;
+//                create and add movie object to vector
+                    movies.push_back(Movie(title, year, stars));
+                } //end of loop over file
+        input_file.close();  //close file
+    } // end of if statment
+
+    return movies;
+}
