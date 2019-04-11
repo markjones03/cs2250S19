4059ab5a1d51f36340dfd72f2ccd81e814c7d60e
markjones03 <46788859+markjones03@users.noreply.github.com>
2019-04-11 09:57:57 -0600

    Create main.cpp

---
+++ mod12/main.cpp
@@ -0,0 +1,71 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/09/2019 03:20:47 PM
+ *       Revision:  none
+ *       Compiler (C):    gcc main.cpp -o main.out -lm
+ *       Compiler (C++):  g++ main.cpp -o main.out -lm
+ *          Usage:  ./main.out 
+ *
+ *         Author:  Hugo Valle (), hugovalle1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+// For C++ Code
+#include <iostream>
+#include <iomanip>
+#include "ItemToPurchase.h"
+using namespace std;
+
+// Main Function
+int main()
+{
+    int i;
+    const int NUM_ITEMS = 2;
+    string name;
+    int price;
+    int quantity;
+    double total;
+    ItemToPurchase item1;
+    ItemToPurchase item2;
+
+    for (i = 0; i < NUM_ITEMS; ++i) 
+    {
+        cout << "Item " << i + 1 << endl;
+        cout << "Enter the item name:" << endl;
+        getline(cin, name);
+
+        cout << "Enter the item price:" << endl;
+        cin >> price;
+
+        cout << "Enter the item quantity:" << endl;
+        cin >> quantity;
+        cout << endl;
+
+        // Set the Object values for 1
+
+        // Set the Object values for 2
+
+
+
+        cin.ignore();
+    }
+
+    // Calculate the total
+
+    // Print Total
+    cout << "TOTAL COST" << endl;
+    item1.Print();
+    item2.Print();
+    cout << endl;
+    cout << "Total: $" << total << endl;
+
+
+    return 0;
+}
