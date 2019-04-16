c5d67460fd5fff0debbe36fd7ee97ef976b1e1ce
Mark Jones <markjones7@mail.weber.edu>
2019-04-16 09:56:01 -0600

    added delete first and last members of the list

--- mod14/List.cpp
+++ mod14/List.cpp
@@ -56,26 +56,90 @@ void List::display()
 }
 
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  insert_start
+ *  Description:  Insert a node at the begining of the list. remapped your head pointer
+ * =====================================================================================
+ */
 void List::insert_start(int value)
 {
+    //create the new node
+    node *temp = new node;
+    temp->data = value;
+    temp->next =head;
+    // Set the head pointer
+    head = temp;
     return;
 }
 
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  insert_position
+ *  Description:  insert at a given index position then, reconnect the list
+ * =====================================================================================
+ */
 void List::insert_position(int pos, int value)
 {
+    node *prev = new node;
+    node *temp = new node;
+    node *cur = new node;
+    
+    cur = head; // point to first member
+    for(int i = 1; i < pos; i++)
+    {
+        prev = cur;
+        cur = cur->next;
+    }
+    // Connect prev to current to rest of the list
+    temp->data = value;
+    prev->next = temp;
+    temp->next = cur;
     return;
 }
 
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  delete_first
+ *  Description:  delete first member then remapped the head
+ * =====================================================================================
+ */
 void List::delete_first()
 {
+    node *temp = new node;
+    temp = head; // save old head
+    head = head->next;
+
+    delete temp; //free this memory
     return;
 }
 
 
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  delete_last
+ *  Description:  delete last member of list, set new last member to NULL
+ * =====================================================================================
+ */
 void List::delete_last()
 {
+    node *current = new node;
+    node *prev = new node;
+
+    current = head; // point at first
+    //Loop until the end
+    while(current->next != NULL)
+    {
+        prev = current;
+        current = current->next;
+    }
+    // set last member
+    tail = prev;
+    prev->next = NULL;
+
+    delete current; //free this memory
     return;
 }
 
